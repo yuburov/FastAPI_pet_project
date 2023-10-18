3019d92e2a1fcb2610db34d7da61a2cec99a67d4
Yuburov <yubur.aziz@gmail.com>
2023-10-18 13:39:43 +0600

    deleted unnecessary code in api/users.py

--- src/api/users.py
+++ src/api/users.py
@@ -1,26 +1,4 @@
-# from fastapi import APIRouter, Depends, Security
-#
-# from src.api.dependencies import UOWDep
-# from src.config import JWT_SECRET_KEY
-# from src.schemas.users import ResponseSchema
-# from src.repositories.auth_repo import JWTBearer, JWTRepo
-# from fastapi.security import HTTPAuthorizationCredentials
-# from src.services.users import UsersService
-#
-# router = APIRouter(
-#     prefix="/users",
-#     tags=['user'],
-#     dependencies=[Depends(JWTBearer())]
-# )
-#
-#
-# @router.get("/", response_model=ResponseSchema, response_model_exclude_none=True)
-# async def get_user_profile(uow: UOWDep, credentials: HTTPAuthorizationCredentials = Security(JWTBearer())):
-#     print(credentials)
-#     token = JWTRepo.extract_token(credentials, JWT_SECRET_KEY)
-#     result = await UsersService.get_user_profile(uow, token['username'])
-#     return ResponseSchema(detail="Successfully fetch data!", result=result)
-from fastapi import APIRouter, Depends, Security
+from fastapi import APIRouter, Security
 
 from src.api.dependencies import UOWDep
 from src.config import JWT_SECRET_KEY
